a2782bf0ed49170012b44e9fb27f086defe004c1
Orlando Nguyen <orlando.nguyen@uni-due.de>
2021-02-23 14:58:54 +0100

    Create new Dockerfiles

--- build_images.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t a3p_api -f BackendAPIDockerfile .
-docker build -t a3p_services -f BackendServicesDockerfile .
-docker build -t a3p_frontend -f FrontendDockerfile .
